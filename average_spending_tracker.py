b654d363d741a958c4bba0a20a60cc1716941bd8
settarovartur <settarovartur2@gamil.com>
2025-05-23 22:39:37 +0500

    bajarldi

--- average_spending_tracker.py
+++ average_spending_tracker.py
@@ -1,1 +1,14 @@
+from num2words import num2words
 
+
+kun_1 = float(input("1-kun xarajatizni kiriting :"))
+kun_2 = float(input("2-kun xarajatizni kiriting :"))
+kun_3 = float(input("3-kun xarajatizni kiriting :"))
+kun_4 = float(input("4-kun xarajatizni kiriting :"))
+kun_5 = float(input("5-kun xarajatizni kiriting :"))
+kun_6 = float(input("6-kun xarajatizni kiriting :"))
+kun_7 = float(input("7-kun xarajatizni kiriting :"))
+
+ortacha_xarajat =(kun_1+kun_2 + kun_3 + kun_4 +kun_5 + kun_6 + kun_7) / 7
+
+print(ortacha_xarajat)

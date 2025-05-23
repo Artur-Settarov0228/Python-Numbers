78c92ab1a2b6395009f7587c96b9682015139673
settarovartur <settarovartur2@gamil.com>
2025-05-23 21:23:14 +0500

    bajartildi

--- delivery_service_price.py
+++ delivery_service_price.py
@@ -1,1 +1,12 @@
+from num2words import num2words
+
+
+km = float(input("kmni kiriting :"))
+boshlangich_tolov = 5.00
+har_bir_km_uchun = 0.80
+jami = km * har_bir_km_uchun+ boshlangich_tolov
+
+print(jami)
+print(num2words(jami , lang="ru"))
+print(num2words(jami , lang="en"))
 

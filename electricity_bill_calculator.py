c7f971b42e1f2cb014e127f105f1f4bde5783c8b
settarovartur <settarovartur2@gamil.com>
2025-05-23 19:55:25 +0500

    Bajarildi

--- electricity_bill_calculator.py
+++ electricity_bill_calculator.py
@@ -1,1 +1,10 @@
+from num2words import num2words
 
+narx_kw = 0.45
+oy_boshi = float(input("oy boshidagi kwh ni kiriting :"))
+oy_oxiri = float(input("oy oxiridagi kwh ni kiriting :"))
+jami = (oy_oxiri - oy_boshi) * 0.45
+print(jami)
+
+print(num2words(jami , lang="en"))
+print(num2words(jami , lang="ru"))

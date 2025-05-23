c5f89841d988214b123f88f04b9eaeb3c566bcd0
settarovartur <settarovartur2@gamil.com>
2025-05-23 22:24:43 +0500

    bajarldi

--- taxi_fare_calculator.py
+++ taxi_fare_calculator.py
@@ -1,1 +1,13 @@
+from num2words import num2words
+from decimal import Decimal
 
+boshlangin_narxi = Decimal("3.00")
+km_1 = Decimal("1.20")
+km = Decimal(input("km ni kiriting :"))
+taxi_narxi = (km * km_1) + boshlangin_narxi
+
+print(taxi_narxi)
+
+print(num2words( taxi_narxi , lang='ru') )
+
+print(num2words( taxi_narxi , lang='en') )

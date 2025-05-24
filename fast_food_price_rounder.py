46297d725aef9882043d58a869155e6a4fd282a7
settarovartur <settarovartur2@gamil.com>
2025-05-24 10:16:20 +0500

    bajarildi

--- fast_food_price_rounder.py
+++ fast_food_price_rounder.py
@@ -1,1 +1,11 @@
+from num2words import num2words
 
+
+narx_1 = float(input("narxni kiriting :"))
+narx_2 = float(input("narxni kiriting :"))
+narx_3 = float(input("narxni kiriting :"))
+
+jami_narxi = narx_1 + narx_2 + narx_3 
+print(jami_narxi)
+print(num2words(jami_narxi , lang="en"))
+print(num2words(jami_narxi , lang="ru"))
